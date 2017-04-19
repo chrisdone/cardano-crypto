61648fc1f7579ec567891f775451349f2a96917d
Chris Done <chrisdone@gmail.com>
2017-04-19 13:29:49 +0100

    Add weigh code

---
+++ test/Main.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE BangPatterns #-}
+
+module Main where
+
+import qualified Cardano.Crypto.Wallet as CC
+import qualified Cardano.Crypto.Wallet.Encrypted as CC
+import           Data.ByteArray
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString as S
+import           Weigh
+
+main =
+  mainWith
+    (sequence
+       [ func
+         ("CC.generate: " ++ show n)
+         (flip CC.generate (mempty :: ScrubbedBytes))
+         s
+       | (n, !s) <-
+           map (\n -> (n, S.replicate n 0)) [0, 1, 10, 100, 1000, 10000]
+       ])
